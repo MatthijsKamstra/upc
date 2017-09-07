87311e0f4959882272dfe5ca957578ae26f40699
Matthijs Kamstra <matthijs@fonk.amsterdam>
2017-09-07 20:34:01 +0200

    initial commit

---
+++ src/model/constants/App.hx
@@ -0,0 +1,16 @@
+package model.constants;
+
+import haxe.macro.Context;
+
+class App {
+
+	public static inline var URL : String  = "https://";
+
+	public static var BUILD : String = getBuildDate();
+
+	macro public static function getBuildDate() {
+		var date = Date.now().toString();
+		return Context.makeExpr(date, Context.currentPos());
+	}
+
+}
