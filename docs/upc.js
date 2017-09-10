adc53b853f41fb0cae693088353d8aea36700652
Matthijs Kamstra <matthijs@fonk.amsterdam>
2017-09-10 21:30:02 +0200

    change to me and papertoys

--- docs/upc.js
+++ docs/upc.js
@@ -47,9 +47,9 @@ Main.main = function() {
 };
 Main.prototype = {
 	initTwitter: function() {
-		var configProfile = { "profile" : { "screenName" : "jason_mayes"}, "domId" : "example1", "maxTweets" : 1, "enableLinks" : true, "showUser" : true, "showTime" : true, "showImages" : false, "lang" : "en"};
+		var configProfile = { "profile" : { "screenName" : "matthijskamstra"}, "domId" : "example1", "maxTweets" : 5, "enableLinks" : true, "showUser" : true, "showTime" : true, "showImages" : false, "lang" : "en"};
 		twitterFetcher.fetch(configProfile);
-		var config2 = { "id" : "347099293930377217", "domId" : "example2", "maxTweets" : 5, "enableLinks" : true, "showUser" : true, "showTime" : true, "lang" : "en"};
+		var config2 = { "id" : "906962614038786053", "domId" : "example2", "maxTweets" : 5, "enableLinks" : true, "showUser" : true, "showTime" : true, "lang" : "en"};
 		twitterFetcher.fetch(config2);
 	}
 	,init: function() {
@@ -643,7 +643,7 @@ if(ArrayBuffer.prototype.slice == null) {
 var Uint8Array = $global.Uint8Array || js_html_compat_Uint8Array._new;
 js_Boot.__toStr = ({ }).toString;
 js_html_compat_Uint8Array.BYTES_PER_ELEMENT = 1;
-model_constants_App.BUILD = "2017-09-10 21:12:18";
+model_constants_App.BUILD = "2017-09-10 21:29:29";
 Main.main();
 })(typeof window != "undefined" ? window : typeof global != "undefined" ? global : typeof self != "undefined" ? self : this);
 
